a7a6836ac1fad4c60f1bf7e33a4b210b9763e331
VasylArtymovych <art777vasss@gmail.com>
2022-07-03 20:48:53 +0300

    add start files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import { Box } from "components/Box/Box";
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <Box> 
+    
+      </Box>
+    )
+  }
+};
+
+export default App;
+
