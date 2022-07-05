159fe502872c230aafec04efc2f8478d485c2b03
VasylArtymovych <art777vasss@gmail.com>
2022-07-05 20:38:38 +0300

    update settings

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,10 @@
-import { StyledButton } from "./Button.styled"
+import { StyledButton } from './Button.styled';
 import PropTypes from 'prop-types';
 
-export const Button = ({onClick}) => {
-    return (
-        <StyledButton onClick={onClick}>Load more</StyledButton>
-    )
+export const Button = ({ onClick }) => {
+  return <StyledButton onClick={onClick}>Load more</StyledButton>;
 };
 
 Button.propTypes = {
-    onClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
